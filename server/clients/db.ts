4fab730d7290e9dfb9671d6a70d5e6bc1dfe48ae
Bruce Röttgers <bruce@roettgers.co>
2025-03-04 10:37:53 +0100

    try a delay

--- server/clients/db.ts
+++ server/clients/db.ts
@@ -1,6 +1,12 @@
 import mysql, { Connection } from "mysql2/promise";
 
 // Create the connection pool. The pool-specific settings are the defaults
-const connection = mysql.createConnection(process.env.DATABASE_URL!);
+const connection = new Promise<Connection>((res, rej) =>
+  setTimeout(
+    () =>
+      mysql.createConnection(process.env.DATABASE_URL!).then(res).catch(rej),
+    20000,
+  ),
+);
 
 export default connection;
