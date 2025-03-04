049ae1731302aa95a642e107420362c55e5586df
Bruce Röttgers <bruce@roettgers.co>
2025-03-04 11:14:40 +0100

    logging

--- server/clients/db.ts
+++ server/clients/db.ts
@@ -2,11 +2,16 @@ import mysql, { Connection } from "mysql2/promise";
 
 // Create the connection pool. The pool-specific settings are the defaults
 const connection = new Promise<Connection>((res, rej) =>
-  setTimeout(
-    () =>
-      mysql.createConnection(process.env.DATABASE_URL!).then(res).catch(rej),
-    20000,
-  ),
+  setTimeout(() => {
+    console.log("timeout");
+    mysql
+      .createConnection(process.env.DATABASE_URL!)
+      .then((conn) => {
+        console.log("connected");
+        res(conn);
+      })
+      .catch(rej);
+  }, 20000),
 );
 
 export default connection;
