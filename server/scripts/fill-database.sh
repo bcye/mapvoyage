13ecbdae86d99137d05c261037e539599a54b3c1
Bruce Röttgers <bruce@roettgers.co>
2025-03-04 11:48:59 +0100

    oops

--- server/scripts/fill-database.sh
+++ server/scripts/fill-database.sh
@@ -10,5 +10,5 @@ mkdir -p /tmp/wiki-import
 wget -O /tmp/wiki-import/page_props.sql.gz https://dumps.wikimedia.org/enwikivoyage/latest/enwikivoyage-latest-page_props.sql.gz
 wget -O /tmp/wiki-import/page.sql.gz https://dumps.wikimedia.org/enwikivoyage/latest/enwikivoyage-latest-page.sql.gz
 
-zcat /tmp/wiki-import/page.sql.gz | docker compose exec -T db mysql -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" "$MARIADB_DATABASE"
-zcat /tmp/wiki-import/page_props.sql.gz | docker compose exec -T db mysql -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" "$MARIADB_DATABASE"
+zcat /tmp/wiki-import/page.sql.gz | docker compose exec -T db mariadb -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" "$MARIADB_DATABASE"
+zcat /tmp/wiki-import/page_props.sql.gz | docker compose exec -T db mariadb -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" "$MARIADB_DATABASE"
