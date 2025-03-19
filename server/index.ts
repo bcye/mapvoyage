67a04589281fb4b0dfe81f20e8a6034212f6b215
Bruce Röttgers <hi+gh@bruceroettgers.eu>
2025-03-19 18:16:23 +0100

    update script?

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,11 @@
 import * as BunnySDK from "@bunny.net/edgescript-sdk";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "./router.js";
+import * as maptiler from "@maptiler/client";
 
 BunnySDK.net.http.serve((req) => {
+  maptiler.config.apiKey = process.env.MAPTILER_API_KEY!;
+
   return fetchRequestHandler({
     endpoint: "/",
     req,
--- server/router.ts
+++ server/router.ts
@@ -1,9 +1,5 @@
-import * as maptiler from "@maptiler/client";
 import { router } from "./trpc.js";
 import getPage from "./procedures/get-page.js";
-import process from "node:process";
-
-maptiler.config.apiKey = process.env.MAPTILER_API_KEY!;
 
 export const appRouter = router({
   getPage,
