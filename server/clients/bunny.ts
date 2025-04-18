7d952b92089cb6bbbecfc420ffc31a8179dd1a72
Bruce Röttgers <hi@bruceroettgers.eu>
2025-04-18 17:44:54 +0200

    only check exists and return id

--- server/clients/bunny.ts
+++ server/clients/bunny.ts
@@ -1,20 +1,26 @@
+import { RootNode } from "../types/nodes.js";
 import { PageInfo } from "../types/wikivoyage.js";
 
-const BASE_URL = "https://mapvoyage.b-cdn.net";
+const BASE_URL = "https://cdn.infra.mapvoyage.app";
 
 export function getObject(path: string) {
   return fetch(`${BASE_URL}/${path}`);
 }
 
-export function getWikiItem<DataType extends "nfo" | "wikitext">(
-  id: string,
-  lang: "en",
-  type: DataType,
-): Promise<(DataType extends "nfo" ? PageInfo : string) | null> {
-  return getObject(
-    `${lang}/${id}.${type == "nfo" ? "nfo.json" : "wiki.txt"}`,
-  ).then((r) => {
+export async function wikiItemExists(id: string, lang: "en") {
+  const res = await fetch(`${BASE_URL}/${lang}-json/${id}.wiki.json`, {
+    method: "HEAD",
+  });
+  if (res.ok) return true;
+  if (res.status == 404) return false;
+
+  console.error(res);
+  throw new Error("Weird status");
+}
+
+export function getWikiItem(id: string, lang: "en"): Promise<RootNode | null> {
+  return getObject(`${lang}-json/${id}.wiki.json`).then((r) => {
     if (r.status == 404) return null;
-    else return type == "nfo" ? r.json() : r.text();
+    else return r.json();
   });
 }
