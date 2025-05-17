041356a30eed556a9af06d17d18c640389945577
Bruce Röttgers <hi@bruceroettgers.eu>
2025-05-17 15:53:13 +0200

    refactor tab bar

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -2,8 +2,19 @@ import { PRIMARY_COLOR } from "@/utils/theme";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { Tabs } from "expo-router";
 import { wrap as wrapSentry } from "@sentry/react-native";
+import { IconName } from "@/utils/icon.types";
 
 const TAB_ICON_SIZE = 24;
+
+function getTabBarIcon(name: string, iconName: IconName) {
+  return ({ color }: { color: string }) => (
+    <MaterialCommunityIcons
+      size={TAB_ICON_SIZE}
+      name={iconName}
+      color={color}
+    />
+  );
+}
 
 export default wrapSentry(function TabLayout() {
   return (
@@ -17,39 +28,28 @@ export default wrapSentry(function TabLayout() {
         name="index"
         options={{
           title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              size={TAB_ICON_SIZE}
-              name="map-legend"
-              color={color}
-            />
-          ),
+          tabBarIcon: getTabBarIcon("Explore", "map-legend"),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: getTabBarIcon("Search", "magnify"),
         }}
       />
       <Tabs.Screen
         name="bookmarks"
         options={{
           title: "Bookmarks",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              size={TAB_ICON_SIZE}
-              name="bookmark-multiple"
-              color={color}
-            />
-          ),
+          tabBarIcon: getTabBarIcon("Bookmarks", "bookmark-multiple"),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              size={TAB_ICON_SIZE}
-              name="cog"
-              color={color}
-            />
-          ),
+          tabBarIcon: getTabBarIcon("Settings", "cog"),
         }}
       />
     </Tabs>
