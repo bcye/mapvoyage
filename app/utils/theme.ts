ea5ee24db40a2b4d75dfbba8611e712be06a291a
Bruce Röttgers <hi@bruceroettgers.eu>
2025-05-17 15:45:20 +0200

    add font to project

--- app/utils/theme.ts
+++ app/utils/theme.ts
@@ -1,1 +1,9 @@
+import { ThemeManager } from "react-native-ui-lib";
+
 export const PRIMARY_COLOR = "#388659";
+
+export function initialiseTheme() {
+  ThemeManager.setComponentTheme("Text", (props: any) => ({
+    style: [{ fontFamily: "Fraunces" }, props.style],
+  }));
+}
