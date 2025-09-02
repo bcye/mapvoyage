d835b3b884d81c75224d79fa48b215324c6e1395
Bruce Röttgers <git@bruceroettgers.eu>
2025-09-02 09:57:22 +0200

    set maptiler key in dev server too

--- server/dev.ts
+++ server/dev.ts
@@ -1,9 +1,11 @@
 import { appRouter } from "./router.js";
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import process from "node:process";
+import * as maptiler from "@maptiler/client";
 
 // Export type router type signature,
 // NOT the router itself. -> otherwise server code might be visible from client bundle
+maptiler.config.apiKey = process.env.MAPTILER_API_KEY!;
 
 const server = createHTTPServer({
   router: appRouter,
