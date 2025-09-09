4a522a51208433b6306e50774fced1776000fab6
Bruce Röttgers <git@bruceroettgers.eu>
2025-09-09 13:08:05 +0200

    v0.14

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   const baseConfig: ExpoConfig = {
     name: "Mapvoyage",
     slug: "mapvoyage",
-    version: "0.13.0",
+    version: "0.14.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "myapp",
